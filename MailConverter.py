650839041233a9502826cd2bae23a966cbade29f
Iason Andriopoulos <iason.andriopoulos@gmail.com>
2016-04-07 21:05:30 +0300

    Final parsing mail script

--- MailConverter.py
+++ MailConverter.py
@@ -1,67 +1,68 @@
 #!/usr/bin/python
 
 from __future__ import division
+from nltk.corpus import stopwords, brown
+from bs4 import BeautifulSoup
 
-import cmd
 import json
 import os
 import re
 import sys
 
 
-class MailConverter(cmd.Cmd):
-    """Simple command processor example."""
+def do_iterate_path(data_path, res_path, category):
+    print("{0} mails...".format(category))
 
-    def __init__(self, path, category):
-        cmd.Cmd.__init__(self)
-        self.path = path
-        self.category = category
+    counter = 0
+    results = []
+    for filename in os.listdir(data_path):
+        try:
+            counter += 1
+            x = BasicMailInformation(data_path + filename, counter, category)
+            x.detailed_parsing(x.crude_parsing())
+            x.process_content()
+            results.append(x)
+        except IOError:
+            pass
+    with open(res_path + "/{0}.json".format(category), 'a') as fp:
+        fp.write('{	"' + category + '":[')
+        first = True
+        for x in results:
+            if first:
+                first = False
+            else:
+                fp.write(",\n")
+            json.dump(x.__dict__, fp)
+        fp.write(']}')
 
-    def do_iterate_path(self, strarg=None):
-        counter = 0
-        for filename in os.listdir(self.path):
-            try:
-                counter += 1
-                x = BasicMailInformation(self.path + filename, counter, self.category)
-                x.detailed_parsing(x.crude_parsing())
-                with open(self.path + '/results/result.json', 'a') as fp:
-                    json.dump(x.__dict__, fp)
-            except IOError as e:
-                pass
 
-    def do_exit(self, strarg=None):
-        """
-        Terminate program.
-        :param strarg: Python cmd specific field
-        """
-        sys.exit(0)
-
-    def do_info(self, strarg=None):
-        """
-        Prints info about mail converter.
-        :param strarg: Python cmd specific field
-        """
-        print("Loads text mails from given directory and exports enhanced information into json file")
-
-    def postloop(self):
-        print
+def get_dictionary():
+    x = {}
+    for word in set(brown.words()):
+        x[word.lower()] = 0
+    print len(x.keys())
+    return x
 
 
 class BasicMailInformation:
     """A Basic email information as acquired by simple parsing"""
 
+    special_chars = ["$", "?", "!", "_"]
+    dictionary = get_dictionary()
+
     def __init__(self, filename, counter, category):
         self.origin_file = filename
         self.counter = counter
         self.category = category
-        self.senders = []
         self.content = ""
         self.subject = ""
-        self.has_attachments = False
-        self.has_history = False
+        self.char_length = 0
+        self.contains_links = False
+        self.num_of_unknown_words = 0
+        self.num_of_special_chars = 0
 
     def print_info(self):
-        print self.__dict__
+        print(self.__dict__)
 
     def crude_parsing(self):
         crude_list = []
@@ -69,7 +70,7 @@ class BasicMailInformation:
                            "content-transfer-encoding:", "x-bcc:", "x-filename", "subject:", "message-id:", "x-origin:"]
         with open(self.origin_file) as f:
             for line in f:
-                line = line.lower()
+                line = line.decode("utf-8", "ignore").encode("utf-8").lower()
                 if line in ['\n', '\r\n']:
                     crude_list.append("content: " + line.strip())
                 else:
@@ -79,20 +80,38 @@ class BasicMailInformation:
                             content = True
                             crude_list.append(line.strip())
                     if not content:
-                        crude_list[len(crude_list)-1] += " " + line.strip()
+                        if len(crude_list) > 0:
+                            crude_list[len(crude_list)-1] += " " + line.strip()
+                        else:
+                            crude_list.append("content: " + line.strip())
         return crude_list
 
     def detailed_parsing(self, crude_list):
         for line in crude_list:
             if line.startswith("content:"):
-                self.content += line.split("content:")[1]
-            if re.match("from:\s+\S+@\S+.\S+", line):
-                self.senders.append(line.split("from:")[1])
-            if line.startswith("x-filename"):
-                self.has_attachments = len(line.split("x-filename")) > 1
+                self.content += " " + line.split("content:")[1]
             if line.startswith("subject:"):
                 self.subject = line.split("subject:")[1].strip()
-        self.has_history = len(self.senders) > 1
+        self.char_length = len(self.content)
+
+    def process_content(self):
+        if re.search("http://", self.content):
+            self.contains_links = True
+        for char in self.content:
+            if char in BasicMailInformation.special_chars:
+                self.num_of_special_chars += 1
+        self.content = BeautifulSoup(self.content, "html.parser").getText()
+        self.content = filter(None, re.split("[ \-\"\t\n\r\f\v';:.?/(),]", self.content))
+        for word in self.content:
+            try:
+                BasicMailInformation.dictionary[word]
+            except Exception:
+                self.num_of_unknown_words += 1
+
+        self.num_of_unknown_words /= (len(self.content) + 1)
+        self.content = [item for item in self.content if item not in stopwords.words('english')]
+
 
 if __name__ == '__main__':
-    MailConverter(sys.argv[1], sys.argv[2]).cmdloop()
+    do_iterate_path(sys.argv[1], sys.argv[3], "ham")
+    do_iterate_path(sys.argv[2], sys.argv[3], "spam")
